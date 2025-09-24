843408bb14cfac0f2064f209dfbfcfb3bf74520e
Adrià Bagüeste <a.bagueste.r@gmail.com>
2025-09-24 17:24:50 +0200

    Auto commit

--- Simulator/Aircraft_lib.py
+++ Simulator/Aircraft_lib.py
@@ -3,13 +3,47 @@ class Aircraft:
     def __init__(self):
         self.Aircraft_list = []
 
-    def add_aircraft(self, Aircraft_type: str, Aircraft_STAR:str, Weight_at_IAF: float):
+    def add_aircraft(self, 
+                     Aircraft_type: str, 
+                     Aircraft_STAR:str, 
+                     Max_landing_weight: float, 
+                     Max_weight: float, 
+                     Max_payload: float, 
+                     Surface: float, 
+                     CD0app: float, 
+                     CD2app: float, 
+                     CD0clean: float, 
+                     CD2clean: float,
+                     hpdesc: int,
+                     CTdesc_high: float,
+                     CTdesc_low: float,
+                     CTdsc_app: float,
+                     CT1: float,
+                     CT2: float,
+                     CT3: float,
+                     CF1: float,
+                     CF2: float):
         
         try:
             Aircraft = {
                 "type": Aircraft_type,
                 "STAR": Aircraft_STAR,
-                "Weight_at_IAF": Weight_at_IAF
+                "Max_landing_weight": Max_landing_weight,
+                "Max_weight": Max_weight,
+                "Max_payload": Max_payload,
+                "CD0app": CD0app,
+                "CD2app": CD2app,
+                "CD0clean": CD0clean,
+                "CD2clean": CD2clean,
+                "hpdesc": hpdesc,
+                "CTdesc_high": CTdesc_high,
+                "CTdesc_low": CTdesc_low,
+                "CTdesc_app": CTdsc_app,
+                "CT1": CT1,
+                "CT2": CT2,
+                "CT3": CT3,
+                "CF1": CF1,
+                "CF2": CF2
             }
 
             self.Aircraft_list.append(Aircraft)
